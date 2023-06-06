9fcf394e55a04975138081d1bcd0682cce692baf
suguna daggumalli <sugunadaggumalli@gmail.com>
2023-06-06 12:58:20 +0530

    updated loaninfo,about

---
+++ loanfront/src/app/loan-info/loan-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-info',
+  templateUrl: './loan-info.component.html',
+  styleUrls: ['./loan-info.component.css']
+})
+export class LoanInfoComponent {
+
+}
